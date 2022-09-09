0c8dfcb2c0829a71fde3ca030026dd21c51f9c9f
Cody Gula <cgula7@gmail.com>
2022-09-08 19:48:38 -0700

    git test commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #----------------------------------------------------------------------------#
 # Imports
 #----------------------------------------------------------------------------#
+# git test change!
 
 from flask import Flask, render_template, request, redirect, flash, url_for
 # from flask.ext.sqlalchemy import SQLAlchemy

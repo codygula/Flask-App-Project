65440224b9502f370ab61dd7968c36ac4bc66ab4
Cody Gula <cgula7@gmail.com>
2022-11-26 13:59:04 -0800

    Starting over the stupid way

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 
-
-
 ## Welcome
 
+Hello. Want to get started with Flask quickly? Good. You came to the right place. This Flask application framework is pre-configured with **Flask-SQLAlchemy**, **Flask-WTF**, **Fabric**, **Coverage**, and the **Bootstrap** frontend (among others). This will get your Flask app up and running on Heroku or PythonAnywhere quickly. Use this starter, boilerplate for all you new Flask projects. Cheers!
 
-Original boilerplate Stolen from Real Python
+<hr>
 
-Original structure
+![real-python-logo](https://raw.githubusercontent.com/realpython/about/master/rp_small.png)
+
+**Designed for the [Real Python](http://www.realpython.com) course.**
+
+<hr>
+
+Preview the skeleton app here - [http://www.flaskboilerplate.com/](http://www.flaskboilerplate.com/)
+
+**EXAMPLE APP: [http://flasktaskr.herokuapp.com/](http://flasktaskr.herokuapp.com/)**
+
+**What is Flask?** Flask is a microframework for Python based on Werkzeug and Jinja2.
+
+Project Structure
+--------
 
   ```sh
   ├── Procfile
@@ -66,7 +78,15 @@ Original structure
           ├── placeholder.about.html
           └── placeholder.home.html
   ```
-# This part doesn't entirely work in practice
+
+### Screenshots
+
+![Pages](https://github.com/realpython/flask-boilerplate/blob/master/screenshots/pages.png)
+
+![Forms](https://github.com/realpython/flask-boilerplate/blob/master/screenshots/forms.png)
+
+
+### Quick Start
 
 1. Clone the repo
   ```
@@ -92,7 +112,6 @@ Original structure
 
 6. Navigate to [http://localhost:5000](http://localhost:5000)
 
-# I haven't tried this part but I'm saving it for possible future use.
 
 Deploying to Heroku
 ------
@@ -154,8 +173,6 @@ Deploying to Heroku
   ```
   $ heroku logs
   ```
-
-# There's no way in hell this will work
 
 ### Deploying to PythonAnywhere
 
@@ -247,6 +264,14 @@ Once the web app has been created (it'll take 20 seconds or so), you'll see a li
 9. Click the green "Add key" button.  You'll be prompted to enter your password.
 
 PUSH and PULL away!
+
+### What's next?
+
+1. Using Heroku? Make sure you deactivate your virtualenv once you're done deploying: `deactivate`
+2. Need to reactivate? (1) Unix - `source venv/bin/activate` (2) Windows - `venv\scripts\activate`
+4. Add your Google Analytics ID to the *main.html* file
+5. Add a domain name to [Heroku](https://devcenter.heroku.com/articles/custom-domains) or PythonAnywhere via a [CNAME](http://en.wikipedia.org/wiki/CNAME_record) record
+5. DEVELOP YOUR APP - need [help](http://realpython.com)?
 
 ### Learn More
 
--- app.py
+++ app.py
@@ -1,25 +1,13 @@
 #----------------------------------------------------------------------------#
 # Imports
 #----------------------------------------------------------------------------#
-# THIS WORKS !!! When saved as app.py
 
-from flask import Flask, render_template, request, redirect, flash, url_for, make_response, session
+from flask import Flask, render_template, request
 # from flask.ext.sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from forms import *
 import os
-import boto3
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
-
-from boto3.dynamodb.conditions import Key, Attr
-
-
-# {% extends 'layouts/main.html' %}
-
-dbclient = boto3.resource("dynamodb")
-table = dbclient.Table("email_list")
-TABLE_NAME = "email_list"
 
 #----------------------------------------------------------------------------#
 # App Config.
@@ -29,17 +17,25 @@ app = Flask(__name__)
 app.config.from_object('config')
 #db = SQLAlchemy(app)
 
-# Flask_Login Stuff
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
+# Automatically tear down SQLAlchemy.
+'''
+@app.teardown_request
+def shutdown_session(exception=None):
+    db_session.remove()
+'''
 
-@login_manager.user_loader
-def load_user(user_id):
-    # Query database
-    # return Users.query.get(int(user_id))
-    print("@login_manager.user_loader called")
-
+# Login required decorator.
+'''
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+'''
 #----------------------------------------------------------------------------#
 # Controllers.
 #----------------------------------------------------------------------------#
@@ -47,7 +43,6 @@ def load_user(user_id):
 
 @app.route('/')
 def home():
-    print("home page entereded!!")
     return render_template('pages/placeholder.home.html')
 
 
@@ -56,145 +51,17 @@ def about():
     return render_template('pages/placeholder.about.html')
 
 
-# Create Login Page
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login')
 def login():
-    print("!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!login function called")
     form = LoginForm(request.form)
-    print("!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!~!!~!~!~!~!~!~login function still working")
-    return render_template("forms/login.html", form=form)
+    return render_template('forms/login.html', form=form)
 
-
-@app.route("/submit/", methods=['POST'])
-def move_forward():
-    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!MOVE FORWARD CALLED!!!")
-    forward_message = "Moving Forward..."
-    form = LoginForm(request.form)
-    if request.method=='POST':
-        print("login thing!!! in /submit/ ")
-   
-        
-        email = form.name.data
-        password = form.password.data
-        session['email_thing'] = email
-        print("email = ", email)
-        print("password = ", password)
-
-        # Check if email and password match database: 
-        
-        response = table.get_item(
-            TableName=TABLE_NAME,
-            Key={
-                "email": email
-            })
-        dbemail = response['Item']['email']
-        dbpassword = response['Item']['passwordhash']
-        
-# TODO: Make dbSearchTerm an array of all search terms and urls from the database for that username. (Read AWS API docs?) It should work in that case.
-#  
-        try:
-            dbSearchTerm = response['Item']['searchterm']
-        except:
-            dbSearchTerm = "none"
-        print("dbpassword = ",  dbpassword)
-        print(response['Item'])
-        
-        # Render dashboard if login successful and login screen again if not. This is somehow actually working.
-
-        if email == dbemail and password == dbpassword:
-            print("LOGIN SUCCESSFUL!!!")
-            return render_template('pages/dashboard.html',utc_dt="testing", emailAddress=email, data=dbSearchTerm, len=len(dbSearchTerm))
-        else:
-            print("LOGIN ERROR!")
-            return render_template("forms/login.html", form=form)
-
-# @app.route("/elements")
-# def elements():
-#     return render_template("elements.html")
-
-@app.route("/set")
-@app.route("/set/<theme>")
-def set_theme(theme="light"):
-    print("-------------------------------->   [[[[[  set_theme called  ]]]]]   <--------")
-    res = make_response(redirect("/dashboard"))
-    # res = make_response(redirect(url_for("dashboard")))
-    res.set_cookie("theme", theme)
-    return res
-# SameSite=None (?)
-
-@app.route('/dashboard', methods =["GET", "POST"])
-#@login_required
-def gfg():
-    email = session.get('email_thing', None)
-    print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ ",email)
-    if request.method == "POST":
-       email = session.get('email_thing', None)
-       print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ ",email)
-       site = request.form.get("site")
-       searchterm = request.form.get("searchterm")
-       # email = request.form.get("email")
-       #messageNumber = request.form.get("messageNumber")
-       
-       print("site = ", site)
-       print("searchterm = ", searchterm)
-       #print("number of messages = ", messageNumber)
-       #print("email = ", email)
-
-       Primary_Column_Name = "email"
-
-       response = table.put_item(
-            Item={
-                Primary_Column_Name: email,
-                "siteURL": site,
-                "searchterm": searchterm,
-                "NOVEMBER_test_item": "testing!!!!!!",
-                })
-       print(response) 
-       print("SUCCESS!") 
-    else:
-        print("gfg() /dashboard ERROR!")
-
-    return render_template('pages/dashboard.html')
-  
 
 @app.route('/register')
 def register():
     form = RegisterForm(request.form)
     return render_template('forms/register.html', form=form)
 
-
-@app.route("/registerSubmit/", methods=['POST'])
-def register_forward():
-    
-    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! /register/ CALLED!!!")
-    forward_message = "Moving Forward..."
-    form = RegisterForm(request.form)
-    if request.method=='POST':
-        print("register if statement in /registerSubmit/!!!!! ")
-        
-        name = form.name.data
-        email = form.email.data
-        password = form.password.data
-        confirm = form.confirm.data
-
-        print("name = ", name)
-        print("email = ", email)
-        print("password = ", password)
-        print("confirm = ", confirm)
-
-        response = table.put_item(
-            Item={
-                "email": email,
-                "fname": name,
-                "passwordhash": password,
-                })
-        print(response) 
-        print("SUCCESS!") 
-
-        return render_template('pages/dashboard.html', forward_message=forward_message);
-
-        
-  
 
 @app.route('/forgot')
 def forgot():
@@ -232,3 +99,9 @@ if not app.debug:
 if __name__ == '__main__':
     app.run()
 
+# Or specify port manually:
+'''
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+'''
